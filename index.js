52da23a82465a87890bdef966559f785be44e759
Andrey Bubenko <a.bubenko@mail.ru>
2022-11-28 18:18:55 +0700

    Сделаны промисы

--- index.js
+++ index.js
@@ -1,51 +1,114 @@
-//import { getCategories, getPostsByPage, getPostsByPageAndCategory } from './request.js';
+import { getCategories, getPostsByPage, getPostsByPageAndCategory} from './request.js';
+let currentPage = 1;
+let currentCategory = "";
 
-//const ulNode = document.querySelector('div');
+const categoriesSelect = document.querySelector(".category-select");
+const nextPageButton = document.querySelector(".pagination__next");
+const prevPageButton = document.querySelector(".pagination__prev");
+const grid = document.querySelector(".grid");
+const mask = document.querySelector(".mask");
 
-let main_div = document.createElement("div");
-main_div.setAttribute("class","grid");
-main_div.textContent("mem");
+/*Сохраняем категории в локал стор*/
+getCategories()
+.then((catrgory) => {
+    localStorage.setItem('catrgories', JSON.stringify(catrgory));
+})
+.then(() =>{
+    addCategoriesToSelect();
+})
+.catch((error) => {
+    console.log(error);
+});
 
-const req = getCategories();
-console.log(req);
+
+/*Заполняем категории из локал стора в выпадающий список*/
+function addCategoriesToSelect(){
+    categoriesSelect.innerHTML = "";
+    let option = document.createElement("option");
+    option.text = "Выберите категорию";
+    option.setAttribute("value","") // Необходимо для "Пустого" выбора.
+    categoriesSelect.appendChild(option);
+    let categories = JSON.parse(localStorage.getItem('catrgories'));
+    categories.forEach(element => {
+        let option = document.createElement("option");
+        option.setAttribute("value",element.id);
+        option.text = element.name;
+        categoriesSelect.appendChild(option);
+    }); 
+}
+
+/*Выбор категории из выпадающего списка*/
+function chooseCurrentCategory(){
+    let categories = JSON.parse(localStorage.getItem('catrgories'));
+    currentCategory = categoriesSelect[categoriesSelect.selectedIndex].value;
+}
+/*Отрисовка постов, при выборе категории*/
+categoriesSelect.addEventListener("change",()=>{
+    chooseCurrentCategory();
+    currentPage=1;
+    getRenderedPosts()
+})
+
+/*Рендер постов, с учетом страницы и категории*/
+ function getRenderedPosts(){
+    mask.classList.remove('hide');
+    return new Promise((resolve) => {
+        if (currentCategory==""){
+            getPostsByPage(currentPage)
+            .then((pages) => {
+                renderPosts(pages);
+                mask.classList.add('hide');
+                return resolve(1)
+            })
+        }
+        else{
+            getPostsByPageAndCategory(currentPage, currentCategory)
+            .then((pages) => {
+                renderPosts(pages);
+                mask.classList.add('hide');
+                return  resolve(1)
+            })
+        }
+      });
+}
+
+/*Первичная отрисовка постов*/
+getRenderedPosts();
+
+/*Функция формирование постов в html структуру*/
+function renderPosts(pages){
+    grid.innerHTML="";
+    pages.forEach(element =>{
+        let post = document.createElement('article');
+        post.classList.add("article")
+        let title = element.title.rendered;
+        let category = element.primary_category.name;
+        let author = element.parselyMeta["parsely-author"]
+        let img = element.parselyMeta["parsely-image-url"];
+        const contentString =
+        `<img class="article__img" src="${img}"/>
+        <div class="article__content">
+            <div class="article__title">${title}</div>
+            <div class="article__category">Primary category: ${category}</div>
+            <div class="article__author">Author: ${author}</div>
+        </div>
+        ` 
+        post.innerHTML = contentString;//element.content.rendered
+        grid.appendChild(post);
+    })
+}
 
 
+/*Пагинация вперед*/
+nextPageButton.addEventListener("click", ()=>{
+    currentPage +=1;
+    getRenderedPosts()
+})
 
-
-
-
-
-
-// const responsePosts = getPosts();
-
-// responsePosts.then((posts) => {
-//     const userIds = new Set(posts.map((item) => item.userId))
-//     buildList(posts)
-//     buildUser(userIds);
-// })
-
-// function buildList(posts)
-// {
-//     var element;
-//     posts.forEach((post) => {
-//         element = document.createElement('li');
-//         element.className = 'list-' + post.userId
-//         element.textContent = post.title
-//         ulNode.append(element)
-//     })
-// }
-
-// function buildUser(ids)
-// {
-//     ids.forEach(id => {
-//         const responseUser = getUserById(id);
-//         responseUser.then((user) => {
-//             document.querySelectorAll(`.list-${user.id}`).forEach((item) => {
-//                 const element = document.createElement('span');
-//                 element.className = 'red'
-//                 element.textContent = user.name
-//                 item.append(element)
-//             })
-//         })
-//     })
-// }
+/*Пагинация назад*/
+prevPageButton.addEventListener("click", ()=>{
+    if(currentPage !=1){
+        currentPage-=1;
+        getRenderedPosts()
+    } 
+})
